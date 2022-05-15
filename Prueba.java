4f02652005618799dd1ad0447ba83503f8157cca
gmejia <98183796+GMejiaR@users.noreply.github.com>
2022-05-15 00:47:11 +0000

    ESTADOS LISTOS

--- Prueba.java
+++ Prueba.java
@@ -1,60 +1,50 @@
 import java.io.*;
 import java.util.*;
 
-public class Gramatica{
-  
+public class Prueba{
   public static char[] listReglas;
   public static char[] listAlfabeto;
   public static int[][] matrizafd;
   
-	public static void main(String[] args) throws Exception{
-		//Su codigo aqui
-    String banderita = args[1];  
-    String nomArchivo = args[2];
-    String ArchGramatica = args[0];
-    
-    if (banderita.equals("-afn")){
-      creador(ArchGramatica);
-      
-    }else{
-      System.out.println("Error: bandera invalida");
-    }
-    
-	}
+  public static void main(String[] args) throws Exception{
 
-  public static void creador(String path) throws Exception{
-    //----------Archivo ----------------------
-    File documento = new File(path);
-    Scanner archivo = new Scanner(documento);
-    //-------------------------------------------
+  
+  String path = args[0];
+  
+  File documento = new File(path);
+  Scanner archivo = new Scanner(documento);  
 
-    
-   //----------Reglas producción ------------ 
-    String linea = archivo.next();
-    String reglas = linea.replace(",", "");
-    listReglas = new char[reglas.length()];
-    
-    for(int i = 0; i < listReglas.length; i++){
-      listReglas[i] = reglas.charAt(i);
-    }
-    //---------------------------------------
+  String linea = archivo.next();
+  String reglas = linea.replace(",", "");
+  listReglas = new char[reglas.length()];
+  
+  for(int i = 0; i < listReglas.length; i++){
+    listReglas[i] = reglas.charAt(i);
+  }  
+  System.out.println("Lista de reglas: " + Arrays.toString(listReglas));
+  //---------------------------------------
 
-    // ------------ Alfabeto ------------
-    String linea2 = archivo.next();
-    String alfabeto = linea2.replace(",", "");
-    listAlfabeto = new char[alfabeto.length()];
-    
-    for(int i = 0; i < listAlfabeto.length; i++){
-      listAlfabeto[i] = alfabeto.charAt(i);
-    }
-    // ----------------------------------
+  // ------------ Alfabeto ------------
+  String linea2 = archivo.next();
+  String alfabeto = linea2.replace(",", "");
+  listAlfabeto = new char[alfabeto.length()];
 
-    // ------------ Símbolo Inicial ------------
-    String linea3 = archivo.next();
-    char SimbInicial = linea3.charAt(0);
-    //-------------------------------------------
+  for(int i = 0; i < listAlfabeto.length; i++){
+    listAlfabeto[i] = alfabeto.charAt(i);
+  }
 
-    // --------- Leer Archivo y Algoritmo ---------
+  System.out.println("Lista de alfabeto leída: " + Arrays.toString(listAlfabeto));
+  // ----------------------------------
+
+  // ------------ Símbolo Inicial ------------
+  String linea3 = archivo.next();
+  char SimbInicial = linea3.charAt(0);
+  System.out.println("Símbolo inicial: " + SimbInicial);
+
+    int pollo = cantidadEstados(path);
+  //-------------------------------------------
+
+     // --------- Leer Archivo y Algoritmo ---------
     int estact = 1;
     int cont = 0;////////CONTADOR---------------------!!!!!!!!!!!!!!!!!!! :D
     ArrayList<Character> patron = reglas(path);
@@ -76,32 +66,13 @@ public class Gramatica{
      
       }
       
-    }  
-    
-  }
-
-  public static boolean verificarRegla(String instruccion){
-    int cont = 0;
-    boolean siono = false;
-    while(cont < instruccion.length()){
-      String posRegla = listReglas[cont].toString();// AVISARLE A BARRIOS :)
-      if(instruccion.substring(instruccion.length()-1,instruccion.length()).equals(posRegla))){
-        siono = true;
-      }
-      cont++;
     }
-    return siono;
-  }
-
-  public static boolean verificarRetornable(String instruccion, String letra){
-    boolean siono = false;
-    if(instruccion.substring(instruccion.length()-1,instruccion.length()).equals(letra)){
-      siono = true;
+    archivo.close();
     }
-    return siono;
-  }
 
-  public static int cantidadEstados(String Path) throws Exception{
+  
+    ////AQUI
+   public static int cantidadEstados(String Path) throws Exception{
     File doc = new File(Path);
     Scanner archivo2 = new Scanner(doc);
     archivo2.next();
@@ -113,24 +84,30 @@ public class Gramatica{
     int estadoss = 2;
     ArrayList<Character> patron = reglas(Path);
     patron.add(' ');
+    patron.add(' ');
     while(archivo2.hasNext()){
       String actual = archivo2.next();
       if (patron.get(cont) == patron.get(cont+1)){
         ArrayList<String> repetidos = new ArrayList<String>();
         repetidos.add(actual);
         while(true){
-          if (patron.get(cont) == patron.get(cont+1)){
-            actual = archivo2.next();
-            repetidos.add(actual);
-            cont++;
+          if(archivo2.hasNext()){
+            if (patron.get(cont) == patron.get(cont+1)){
+              actual = archivo2.next();
+              repetidos.add(actual);
+              cont++;
+            }else{
+              break;
+            }
           }else{
-            break;
-          }
+              break;
+            }
+          
         }
         int ver = 0;
         int tam = repetidos.size();
         while(ver < tam){
-          String inst =(repetidos.get(ver)).substring(2,tam-1);
+          String inst =(repetidos.get(ver)).substring(3,((repetidos.get(ver)).length())-1);
           if(verificarRegla(inst)){
             if(verificarRetornable(inst, actual.substring(0,1))){
               int cantmin = inst.length()-1;
@@ -145,6 +122,7 @@ public class Gramatica{
                 cont++;
               }else if(cantmin == 0){
                 estadoss++;
+                cont++;
               }
             }
           }else{
@@ -158,11 +136,12 @@ public class Gramatica{
         }
           
       }else{
-        String instruccion = actual.substring(2,actual.length());
+        String instruccion = actual.substring(3,actual.length());
         if(verificarRegla(instruccion)){
           if(verificarRetornable(instruccion, actual.substring(0,1))){
             int cantmin = instruccion.length()-1;
             if(cantmin > 1){
+              System.out.println("instr "+instruccion + cantmin);
               estadoss = estadoss + (cantmin-1);
               cont++;
             }
@@ -173,6 +152,7 @@ public class Gramatica{
               cont++;
             }else if(cantmin == 0){
               estadoss++;
+              cont++;
             }
           }
         }else{
@@ -185,9 +165,29 @@ public class Gramatica{
       }
     }
     archivo2.close();
-   
     return estadoss;
-  }
+  }//AQUI
+
+  
+    
+ /* ArrayList<Character> patron = reglas(archLeido);
+  patron.add(' ');
+  int cont = 0;
+  while(cont < patron.size()-1){
+    String instruccion = archivo.next();
+    if (patron.get(cont) == patron.get(cont+1)){
+      System.out.println("hay otra misma regla");
+
+    }
+    else{
+      System.out.println("se pasa a otra regla diferente");
+
+    } 
+    cont+=1;
+    }
+      
+    archivo.close();
+  }*/
 
   public static ArrayList<Character> reglas(String path) throws Exception{
     File documento = new File(path);
@@ -203,7 +203,29 @@ public class Gramatica{
       patron.add(SimbInicial);
     }
     archivo2.close();
-    return patron; 
-  }  
+    return patron;
+    
+  }
 
-}
+  public static boolean verificarRegla(String instruccion){
+    int cont = 0;
+    boolean siono = false;
+    while(cont < instruccion.length()){
+      char posRegla = listReglas[cont];// AVISARLE A BARRIOS :)
+      if((instruccion.substring(instruccion.length()-1,instruccion.length())).charAt(0) == posRegla){
+        siono = true;
+      }
+      cont++;
+    }
+    return siono;
+  }
+
+  public static boolean verificarRetornable(String instruccion, String letra){
+    boolean siono = false;
+    if(instruccion.substring(instruccion.length()-1,instruccion.length()).equals(letra)){
+      siono = true;
+    }
+    return siono;
+  }
+      
+}  
